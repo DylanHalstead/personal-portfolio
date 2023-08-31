b803c9ca1992b81843a18a4075ed4b73e573cfd6
DylanHalstead <dylanhalstead11@gmail.com>
2023-08-31 17:25:43 -0400

    changed axios.get urls

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -13,14 +13,14 @@ import ProjectsSection from '../components/ProjectsSection.vue'
 
 const projects = ref([])
 const getProjects = async () => {
-  await axios.get('http://localhost:5000/api/projects').then((res) => {
+  await axios.get('/api/projects').then((res) => {
     projects.value = res.data
   })
 }
 
 const tags = ref([])
 const getTags = async () => {
-  await axios.get('http://localhost:5000/api/tags').then((res) => {
+  await axios.get('/api/tags').then((res) => {
     tags.value = res.data
   })
 }
